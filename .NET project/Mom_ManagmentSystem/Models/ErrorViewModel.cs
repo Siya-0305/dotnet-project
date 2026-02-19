4207846ffa6f61c18094b114722c5b21834f3c36
siyakansagara <siyakansagara@gmail.com>
2026-02-19 13:31:06 +0530

    Project

--- .NET project/Mom_ManagmentSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mom_ManagmentSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
